390cac320eb3d76b62e7ec89928bf1e8e56d9624
apple-37 <1833346463@qq.com>
2025-08-30 22:43:03 +0800

    fix: change the namespace format

--- src/Application/Interfaces/Services/TicketingSystem/IPromotionService.cs
+++ src/Application/Interfaces/Services/TicketingSystem/IPromotionService.cs
@@ -2,14 +2,13 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using DbApp.Application.DTOs;
 
-namespace DbApp.Application.Interfaces.Services.TicketingSystem
+namespace DbApp.Application.Interfaces.Services.TicketingSystem;
+
+public interface IPromotionService
 {
-    public interface IPromotionService
-    {
-        Task<List<PromotionSummaryDto>> GetAllPromotionsAsync();
-        Task<PromotionDetailDto> GetPromotionDetailAsync(int id);
-        Task<PromotionDetailDto> CreatePromotionAsync(CreatePromotionRequest dto);
-        Task<PromotionDetailDto> UpdatePromotionAsync(int id, UpdatePromotionRequest dto);
-        Task<bool> DeletePromotionAsync(int id);
-    }
+    Task<List<PromotionSummaryDto>> GetAllPromotionsAsync();
+    Task<PromotionDetailDto> GetPromotionDetailAsync(int id);
+    Task<PromotionDetailDto> CreatePromotionAsync(CreatePromotionRequest dto);
+    Task<PromotionDetailDto> UpdatePromotionAsync(int id, UpdatePromotionRequest dto);
+    Task<bool> DeletePromotionAsync(int id);
 }
