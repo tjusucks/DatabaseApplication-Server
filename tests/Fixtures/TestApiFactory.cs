e0d52623bbf44b7ac42d89f3829547da19cfeb62
2edbef4a9b <echofedn@outlook.com>
2025-09-02 21:34:08 +0800

    Implement DatabaseFixture for integration tests

---
+++ tests/Fixtures/TestApiFactory.cs
@@ -0,0 +1,26 @@
+using DbApp.Infrastructure;
+using DbApp.Presentation;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace DbApp.Tests.Fixtures;
+
+public class TestApiFactory(DatabaseFixture fixture) : WebApplicationFactory<Program>
+{
+    private readonly DatabaseFixture _fixture = fixture;
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            // Remove existing DbContext.
+            services.RemoveAll<DbContextOptions<ApplicationDbContext>>();
+            services.RemoveAll<ApplicationDbContext>();
+
+            // Inject fixture's DbContext as singleton.
+            services.AddSingleton(_fixture.DbContext);
+        });
+    }
+}
