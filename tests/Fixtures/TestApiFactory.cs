5a02c18d2e211f4a9683e6493c5022756e5be87c
Guwener520 <3251104506@qq.com>
2025-09-06 09:23:29 +0800

    Add integration tests related to employee
    Add integration tests related to employee information
    增加员工相关信息的集成测试

--- tests/Fixtures/TestApiFactory.cs
+++ tests/Fixtures/TestApiFactory.cs
@@ -22,5 +22,6 @@ public class TestApiFactory(DatabaseFixture fixture) : WebApplicationFactory<Pro
             // Inject fixture's DbContext as singleton.
             services.AddSingleton(_fixture.DbContext);
         });
+        builder.UseEnvironment("Testing");
     }
 }
